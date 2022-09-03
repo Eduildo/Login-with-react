181b93abf46128f0595cc41890421f2b29fa80d1
eduildo <51817346+Eduildo@users.noreply.github.com>
2022-09-03 21:16:40 +0000

    simple login with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+
+import logo from './assets/logo.svg';
+import './styles.css';
 
 function App() {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="container-login">
+        <div className= "wrap-login">
+          <form className="login-form">
+            <span className="login-form-title">Bem Vindo</span>
+            <span className="login-form-title">
+              <img src={logo} alt="Logotipo"></img>
+            </span>
+            <div className="wrap-input">
+              <input className={email !== "" ? 'has-val input': 'input'} value={email} onChange={e => setEmail(e.target.value)} type="email"/>
+              <span className="focus-input" data-placeholder="Email"></span>
+            </div>
+            <div className="wrap-input">
+              <input className={password !== "" ? 'has-val input': 'input'} value={password} onChange={e => setPassword(e.target.value)} type="password"/>
+              <span className="focus-input" data-placeholder="Password"></span>
+            </div>
+            <div className="container-login-form-btn">
+              <button className="login-form-btn">Entrar</button>
+            </div>
+            <div className="text-center">
+              <span className="txt1">Não possui uma conta? </span>
+              <a className="txt2" href="www.lima.com"> Criar conta </a>
+            </div>
+            <div className="text-center2">
+              <span className="txt">developed by: Eduildo Lima </span>
+            </div>
+          </form>
+        </div>
+        </div>
     </div>
   );
 }
